c07047bc228e0dcf797095d1b315bd5b06e13b76
Alain Paolini <alain.paolini291@gmail.com>
2020-12-03 22:04:34 +0100

    add slider size

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, ContentChildren, Input, QueryList, TemplateRef } from '@angular/core';
-import { trigger, state, style, transition, group, query, animate, animateChild } from '@angular/animations';
+import { trigger, style, transition, group, query, animate } from '@angular/animations';
 import { BooleanInput, coerceBooleanProperty, coerceNumberProperty, NumberInput } from '@angular/cdk/coercion';
 
 import { CarouselItemDirective } from './carousel-item.directive';
@@ -15,15 +15,15 @@ const modulo = (x: number, n: number) => ((x % n) + n) % n;
     trigger('slide', [
       transition('* => next', [
         group([
-          animate('5000ms ease-in-out', style({ transform: 'translateX(calc(-1 * (100% + {{spacing}}px) / ({{n}} + 1)))' })),
-          query(':leave', animate('5000ms ease-in-out'))
+          animate('3000ms ease-in-out', style({ transform: 'translateX(calc(-{{m}} / ({{m}} + {{n}}) * (100% + {{spacing}}px)))' })),
+          query(':leave', animate(3000))
         ])
       ]),
       transition('* => previous', [
-        style({ transform: 'translateX(calc(-1 * (100% + {{spacing}}px) / ({{n}} + 1)))' }),
+        style({ transform: 'translateX(calc(-{{m}} / ({{m}} + {{n}}) * (100% + {{spacing}}px)))' }),
         group([
-          animate('5000ms ease-in-out', style({ transform: 'none' })),
-          query(':leave', animate('5000ms ease-in-out'))
+          animate('3000ms ease-in-out', style({ transform: 'none' })),
+          query(':leave', animate(3000))
         ])
       ])
     ])
@@ -33,41 +33,54 @@ export class CarouselComponent {
   @ContentChildren(CarouselItemDirective, { read: TemplateRef }) items: QueryList<TemplateRef<any>>;
 
   @Input()
+  set cyclic(cyclic: BooleanInput) { this._cyclic = coerceBooleanProperty(cyclic); }
+  get cyclic() { return this._cyclic; }
+  private _cyclic = false;
+
+  @Input()
   set position(position: NumberInput) { this._position = coerceNumberProperty(position, 0); }
   get position() { return this._position; }
   private _position = 0;
 
   @Input()
-  set cyclic(cyclic: BooleanInput) { this._cyclic = coerceBooleanProperty(cyclic); }
-  get cyclic() { return this._cyclic; }
-  private _cyclic = false;
+  set visibleElements(visibleElements: number) { this._position = coerceNumberProperty(visibleElements, 1); }
+  get visibleElements() { return this._visibleElements; }
+  private _visibleElements = 1;
 
-  @Input() visibleElements = 1;
+  @Input()
+  set sliderSize(sliderSize: number) { this._position = coerceNumberProperty(sliderSize, 1); }
+  get sliderSize() { return this._sliderSize; }
+  private _sliderSize = 1;
 
-  @Input() spacing = 0;
+  @Input()
+  set spacing(spacing: number) { this._position = coerceNumberProperty(spacing, 0); }
+  get spacing() { return this._spacing; }
+  private _spacing = 0;
 
   direction: 'previous' | 'next' | null = null;
   animating = false;
 
   previous() {
-    this.position = modulo(+this.position - 1, this.items.length);
+    this.position = modulo(+this.position - this.sliderSize, this.items.length);
     this.direction = 'previous';
   }
 
   next() {
-    this.position = modulo(+this.position + 1, this.items.length);
+    this.position = modulo(+this.position + this.sliderSize, this.items.length);
     this.direction = 'next';
   }
 
   containerWidth() {
     const n = this.visibleElements;
+    const m = this.sliderSize;
     const spacing = this.spacing;
-    return `calc((${(n + 1) * 100}% + ${spacing}px)/${n})`;
+    return `calc((${(m + n) * 100}% + ${m * spacing}px)/${n})`;
   }
 
   itemWidth() {
     const n = this.visibleElements;
+    const m = this.sliderSize;
     const spacing = this.spacing;
-    return `calc((100% - ${n * spacing}px)/${n + 1})`;
+    return `calc((100% - ${(m + n - 1) * spacing}px)/${m + n})`;
   }
 }
